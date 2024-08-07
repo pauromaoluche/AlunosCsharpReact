dce99df2341ab6ae3cc3e06fb1bdb83b04a9d73f
pauluci <pedro.pauluci@elitesoft.com.br>
2024-08-07 12:14:57 -0300

    Retornando primeiros dados

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -1,11 +1,62 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import logoCadastro from "./assets/cadastro.png";
 
 function App() {
+  const baseUrl = "http://localhost:5244/api/alunos";
+
+  const [data, setData] = useState([]);
+  const pedidosGet = async () => {
+    await axios
+      .get(baseUrl)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    pedidosGet();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>teste</h1>
+      <br />
+      <h3>Cadastro de alunos</h3>
+      <header>
+        <img src={logoCadastro} alt="cadastro" />
+        <button className="btn btn-success">Incluir novo aluno</button>
       </header>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Email</th>
+            <th scope="col">Idade</th>
+            <th scope="col">Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((aluno) => (
+            <tr key={aluno.id}>
+              <th>{aluno.id}</th>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button>{" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
