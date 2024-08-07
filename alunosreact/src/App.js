bdb06a766dd5bfbf9e4d9ba06ae90d77e8faa3f7
pauluci <pedro.pauluci@elitesoft.com.br>
2024-08-07 13:58:45 -0300

    Trazendo informacoes e enviando

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -9,6 +9,28 @@ function App() {
   const baseUrl = "http://localhost:5244/api/alunos";
 
   const [data, setData] = useState([]);
+  const [modalIncluir, setModalIncluir] = useState(false);
+
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: "",
+    nome: "",
+    email: "",
+    idade: "",
+  });
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado,
+      [name]: value,
+    });
+    console.log(alunoSelecionado);
+  };
+
   const pedidosGet = async () => {
     await axios
       .get(baseUrl)
@@ -20,17 +42,36 @@ function App() {
       });
   };
 
+  const pedidoPost = async () => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios
+      .post(baseUrl, alunoSelecionado)
+      .then((response) => {
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     pedidosGet();
   }, []);
 
   return (
-    <div className="App">
+    <div className="App container">
       <br />
       <h3>Cadastro de alunos</h3>
       <header>
         <img src={logoCadastro} alt="cadastro" />
-        <button className="btn btn-success">Incluir novo aluno</button>
+        <button
+          className="btn btn-success"
+          onClick={() => abrirFecharModalIncluir()}
+        >
+          Incluir novo aluno
+        </button>
       </header>
       <table className="table table-hover">
         <thead>
@@ -57,6 +98,52 @@ function App() {
           ))}
         </tbody>
       </table>
+
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Adicionar Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label for="nome">Nome: </label>
+            <input
+              id="nome"
+              type="text"
+              className="form-control"
+              name="nome"
+              onChange={handleChange}
+            />
+            <br />
+            <label for="email">Email: </label>
+            <input
+              id="email"
+              type="text"
+              className="form-control"
+              name="email"
+              onChange={handleChange}
+            />
+            <br />
+            <label for="idade">Idade: </label>
+            <input
+              id="idade"
+              type="text"
+              className="form-control"
+              name="idade"
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPost()}>
+            Adicionar
+          </button>{" "}
+          <button
+            className="btn btn-danger"
+            onClick={() => abrirFecharModalIncluir()}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
