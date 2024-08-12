da7d10325d285242a1a3a37b823609efc523d86a
pauluci <pedro.pauluci@elitesoft.com.br>
2024-08-12 13:59:47 -0300

    Validando login

--- clieantreact/src/pages/Login/index.js
+++ clieantreact/src/pages/Login/index.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
 import logoImage from "../../assets/login.png";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useNavigate();
+
+  async function login(event) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await api.post("api/Account/LoginUser", data);
+
+      localStorage.setItem("email", email);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("expiration", response.data.expiration);
+
+      history("/alunos");
+    } catch (error) {
+      alert("O Login falhou " + error);
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImage} alt="Login" id="img1" />
-        <form>
+        <form onSubmit={login}>
           <h1>Cadastro de Alunos</h1>
-          <input placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="button" type="submit">
             Login
           </button>
